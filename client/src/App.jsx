b3fb00c09447c8e3fc1af7ca68e0e31458772986
FerrnA <arriondovfernando@gmail.com>
2022-06-07 09:23:55 -0300

    feat(client/login): ui and state component for login route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import { AppStyled } from "./AppStyled";
 import Layout from "./components/Layout/Layout";
 import Home from "./components/Home/Home";
 import ABM from "./components/ABMoperations/ABM";
+import Login from "./components/Login/Login";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/*" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="operations" element={<ABM />} />
+          <Route path="login" element={<Login />} />
         </Route>
       </Routes>
     </AppStyled>
